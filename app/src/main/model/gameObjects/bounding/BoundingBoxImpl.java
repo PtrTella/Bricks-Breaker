5f0892447a46ab2e58742bd2e4d7b975d38e537a
Fraloha <infofrancesco20@gmail.com>
2023-03-18 01:35:19 +0100

    Refactored collider.

--- app/src/main/model/gameObjects/bounding/BoundingBoxImpl.java
+++ app/src/main/model/gameObjects/bounding/BoundingBoxImpl.java
@@ -3,8 +3,7 @@ package model.main.gameObjects.bounding;
 import javafx.geometry.Rectangle2D;
 
 public abstract BoundingBoxImpl implements BoundingBox {
-
-    //TODO is this object better than the one provided by javafx ? Let's check. 
+    
     private Rectangle2D collider;
 
     public Rectangle2D getCollider() {
