193bd13e3b9640e51f9181f6d08028d8830af1b1
Fraloha <infofrancesco20@gmail.com>
2023-03-18 01:42:56 +0100

    Implemented Controller interface.

--- app/src/main/controllers/stage/Controller.java
+++ app/src/main/controllers/stage/Controller.java
@@ -4,9 +4,13 @@ import main.model.GameState;
 
 public interface Controller {
 
-    void setModel(final GameState state);
+    void setModel(final GameState modelToAttach);
 
     GameState getState();
+
+    void setView(final View viewToAttach);
+
+    View getView();
 
     void init();
 }
