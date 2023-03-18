a1213c9530d97114464a76c28c5907d39696d057
Fraloha <infofrancesco20@gmail.com>
2023-03-18 01:23:29 +0100

    Modified import.

--- app/src/main/model/gameObjects/bounding/BoundingBox.java
+++ app/src/main/model/gameObjects/bounding/BoundingBox.java
@@ -1,6 +1,10 @@
 package main.model.gameObjects.bounding;
 
+import javafx.geometry.Rectangle2D;
+
 public interface BoundingBox {
-    
+
+    public Rectangle2D getCollider();
+
     public boolean isCollidingWith(final BoundingBox boxToCheck);
 }
