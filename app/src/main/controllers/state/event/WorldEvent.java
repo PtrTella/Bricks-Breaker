724d7fe0becf2c805d7e7a9b54eea593c4a8aa7b
Fraloha <infofrancesco20@gmail.com>
2023-03-18 11:58:45 +0100

    Code style refactor

--- app/src/main/controllers/state/event/WorldEvent.java
+++ app/src/main/controllers/state/event/WorldEvent.java
@@ -3,5 +3,7 @@ package main.controllers.state.event;
 import main.model.GameState;
 
 public interface WorldEvent {
-    void process(GameState currentGame);    
+
+    void process(GameState currentGame);
+    
 }
