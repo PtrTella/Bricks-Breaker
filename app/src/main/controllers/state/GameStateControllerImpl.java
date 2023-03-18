aa96c9ec16bc81aad397cc6316317dc6f6808626
Fraloha <infofrancesco20@gmail.com>
2023-03-18 12:17:49 +0100

    Implemented process event method

---
+++ app/src/main/controllers/state/GameStateControllerImpl.java
@@ -0,0 +1,50 @@
+package main.controllers.state;
+
+import main.controllers.ControllerImpl;
+import main.controllers.state.event.WorldEventListener;
+import main.controllers.state.event.WorldEventListenerImpl;
+
+public class GameStateControllerImpl extends ControllerImpl implements GameStateController {
+
+    private WorldEventListener eventListener;
+
+    @Override
+    public void processCommands() {
+
+    }
+
+    @Override
+    public void processEvents() {
+        this.eventListener.processAll();
+    }
+
+    @Override
+    public void render() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'render'");
+    }
+
+    @Override
+    public void updateGame(int elapsed) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateGame'");
+    }
+
+    @Override
+    public void quitGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'quitGame'");
+    }
+
+    @Override
+    public void pauseGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pauseGame'");
+    }
+
+    @Override
+    public void init() {
+        this.eventListener = new WorldEventListenerImpl();
+    }
+    
+}
