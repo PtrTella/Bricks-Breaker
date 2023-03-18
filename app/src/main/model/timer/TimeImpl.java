68e2249a90dfbcb762167abcfe1ae513be3a8329
Fraloha <infofrancesco20@gmail.com>
2023-03-18 14:28:29 +0100

    Implemented TimeImpl class.

---
+++ app/src/main/model/timer/TimeImpl.java
@@ -0,0 +1,31 @@
+package main.model.timer;
+
+public class TimeImpl implements Time {
+
+    private static final int SECONDS_IN_MINUTE = 60;
+    private int total;
+
+    public TimeImpl(final int time) {
+        this.setTotal(time);
+    }
+
+    @Override
+    public void setTotal(int totalTime) {
+        this.total = totalTime;
+    }
+
+    @Override
+    public int getTotal() {
+        return this.total;    
+    }
+
+    @Override
+    public int getMinutes() {
+        return this.total / SECONDS_IN_MINUTE;
+    }
+
+    @Override
+    public int getSecondsInMinute() {
+        return this.total % SECONDS_IN_MINUTE;
+    }
+}
