218fa89b7754540482f3c37aeac14876f2287587
Fraloha <infofrancesco20@gmail.com>
2023-03-20 14:25:48 +0100

    Defined and implemented view class.

---
+++ app/src/main/view/ViewImpl.java
@@ -0,0 +1,34 @@
+package main.view;
+
+import main.controllers.Controller;
+
+public abstract class ViewImpl implements View {
+
+    private Controller currentController;
+
+    public ViewImpl(final Controller controllerToAttach) {
+        this.setController(controllerToAttach);
+    }
+
+    /**
+     * {@inheritDoc}}
+     */
+    @Override
+    public void setController(final Controller controllerToAttach) {
+        this.currentController = controllerToAttach;
+    }
+
+    /**
+     * {@inheritDoc}}
+     */
+    @Override
+    public Controller getController() {
+        return this.currentController;
+    }
+
+    /**
+     * {@inheritDoc}}
+     */
+    @Override
+    public abstract void init();
+}
