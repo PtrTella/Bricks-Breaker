af63e373c857428bc26c6a46b07147ec1fbcf874
Luca Bighini <bighini.luca@gmail.com>
2023-03-20 16:35:51 +0100

    clean Brick and create PowerUp

--- app/src/main/model/gameObjects/Brick.java
+++ app/src/main/model/gameObjects/Brick.java
@@ -1,24 +1,12 @@
 package main.model.gameObjects;
 
-import main.common.P2d;
-import main.common.V2d;
-import main.model.BoundingBox;
-
-public class Brick extends GameObjectImpl {
+public class Brick {
 
     private int life;
-    private int powerUp;    // contiene il valore del powerup nella enum (0 se assente)
+    private int powerUp;
 
-    public Brick(Type type, P2d pos, V2d vel, BoundingBox box, int life, int powerUp) {
-        super(type, pos, vel, box);
-        this.life = life;
-        this.powerUp = powerUp;
-    }
-
-    @Override
-    public void update(long dt) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    public Brick() {
+        
     }
     
 }
---
+++ app/src/main/model/gameObjects/PowerUp.java
@@ -0,0 +1,5 @@
+package main.model.gameObjects;
+
+public class PowerUp {
+    
+}
