4295bee14ca1fc5deb66c3dff26b921d4a30a1f2
Fraloha <infofrancesco20@gmail.com>
2023-03-20 14:40:54 +0100

    Added getScore() method.

--- app/src/main/controllers/state/GameStateController.java
+++ app/src/main/controllers/state/GameStateController.java
@@ -2,17 +2,45 @@ package main.controllers.state;
 
 import main.controllers.Controller;
 
+/**
+ * This interface defines the game state controller.
+ */
 public interface GameStateController extends Controller {
 
+    /**
+     * This method processes all the commands triggered by the user.
+     */
     void processCommands();
 
+    /**
+     * This method processes all the world events.
+     */
     void processEvents();
 
+    /**
+     * This method renders the attached view.
+     */
     void render();
 
+    /**
+     * This method updates the current Game.
+     * @param elapsed
+     */
     void updateGame(final int elapsed);
 
+    /**
+     * This method closes the game.
+     */
     void quitGame();
 
+    /**
+     * This method pauses the current game.
+     */
     void pauseGame();
+
+    /**
+     * This method gets the current game score.
+     * @return an integer value that represents the score.
+     */
+    int getScore();
 }
--- app/src/main/controllers/state/GameStateControllerImpl.java
+++ app/src/main/controllers/state/GameStateControllerImpl.java
@@ -15,23 +15,35 @@ public class GameStateControllerImpl extends ControllerImpl implements GameState
         this.pause = false;
         this.quit = false;
     }
-    
+
+    /**
+     * {@inheritDoc}}
+     */
     @Override
     public void processCommands() {
 
     }
 
+    /**
+     * {@inheritDoc}}
+     */
     @Override
     public void processEvents() {
         this.eventListener.processAll();
     }
 
+    /**
+     * {@inheritDoc}}
+     */
     @Override
     public void render() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'render'");
     }
 
+    /**
+     * {@inheritDoc}}
+     */
     @Override
     public void updateGame(final int elapsed) {
         this.getModel().updateGame(elapsed);
@@ -41,21 +53,38 @@ public class GameStateControllerImpl extends ControllerImpl implements GameState
         this.processEvents();
     }
 
+    /**
+     * {@inheritDoc}}
+     */
     @Override
     public void quitGame() {
         this.getModel().setGameOver(true);
         this.quit = true;
     }
 
+    /**
+     * {@inheritDoc}}
+     */
     @Override
     public void pauseGame() {
         this.pause = !this.pause;
     }
 
+    /**
+     * {@inheritDoc}}
+     */
     @Override
     public void init() {
         this.eventListener = new WorldEventListenerImpl();
         this.eventListener.setGameState(getModel());
+    }
+
+    /**
+     * {@inheritDoc}}
+     */
+    @Override
+    public int getScore() {
+        return this.getModel().getScore();
     }
     
 }
