6d9bd4358a7d454c9cc2318a8544dcf3615da2b1
Fraloha <infofrancesco20@gmail.com>
2023-03-20 15:26:04 +0100

    Added some method to the World interface.

--- app/src/main/model/World.java
+++ app/src/main/model/World.java
@@ -1,5 +1,8 @@
 package main.model;
 
+import java.util.List;
+import main.model.gameObjects.Ball;
+import main.model.gameObjects.Brick;
 import main.controllers.state.event.WorldEventListener;
 
 public interface World {
@@ -13,4 +16,8 @@ public interface World {
     void checkRespawn();
 
     void checkBoundary();
+
+    List<Brick> getBricks();
+
+    List<Ball> getBalls();
 }
