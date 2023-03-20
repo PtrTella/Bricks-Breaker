4e01419a42de55fec93ae16acc90dba50c7d1a85
Fraloha <infofrancesco20@gmail.com>
2023-03-20 15:25:27 +0100

    Implemented run method and some refactor.

--- app/src/main/controllers/state/GameStateControllerImpl.java
+++ app/src/main/controllers/state/GameStateControllerImpl.java
@@ -4,11 +4,14 @@ import main.controllers.ControllerImpl;
 import main.controllers.state.event.WorldEventListener;
 import main.controllers.state.event.WorldEventListenerImpl;
 
-public class GameStateControllerImpl extends ControllerImpl implements GameStateController {
+public class GameStateControllerImpl extends ControllerImpl implements GameStateController, Runnable {
+
+    private static final double PERIOD = 16.6666;
 
     private WorldEventListener eventListener;
     private boolean pause;
     private boolean quit;
+    private Thread game;
 
     public GameStateControllerImpl() {
         super();
@@ -77,6 +80,12 @@ public class GameStateControllerImpl extends ControllerImpl implements GameState
     public void init() {
         this.eventListener = new WorldEventListenerImpl();
         this.eventListener.setGameState(getModel());
+        this.getModel().init();
+        this.getModel().getWorld().setEventListener(this.eventListener);
+        this.game = new Thread(this);
+        this.game.setName("GameLoop");
+        this.game.start();
+        this.getModel().getGameTimerThread().start();
     }
 
     /**
@@ -85,6 +94,35 @@ public class GameStateControllerImpl extends ControllerImpl implements GameState
     @Override
     public int getScore() {
         return this.getModel().getScore();
+    }
+
+    @Override
+    public void run() {
+        long last = System.currentTimeMillis();
+
+        while(!this.getModel().isGameOver()) {
+            long current = System.currentTimeMillis();
+            int elapsed = (int) (current - last);
+            this.processCommands();
+            this.updateGame(elapsed);
+            this.render();
+            this.waitUntilNextFrame(current);
+            last = current;
+        }
+
+        this.getModel().getGameTimerThread().stopTimer();
+        //TODO: Add the user to a rank.
+    }
+
+    public void waitUntilNextFrame(final long currentFrame) {
+        long dt = System.currentTimeMillis() - currentFrame;
+        if (dt < GameStateControllerImpl.PERIOD) {
+            try {
+                Thread.sleep((long) PERIOD - dt);
+            } catch(Exception e) {
+
+            }
+        }
     }
     
 }
