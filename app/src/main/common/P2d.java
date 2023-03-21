dc1bc974bcddbdaceb11cd6a7cfc88a6b92238d7
Fraloha <infofrancesco20@gmail.com>
2023-03-21 14:29:56 +0100

    Added getX() and getY().

--- app/src/main/common/P2d.java
+++ app/src/main/common/P2d.java
@@ -5,19 +5,28 @@ package main.common;
  */
 public class P2d implements java.io.Serializable {
 
-    public double x,y;
+    private double x;
+    private double y;
 
-    public P2d(double x,double y){
-        this.x=x;
-        this.y=y;
+    public P2d(final double x, final double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
     }
 
     public P2d sum(V2d v){
-        return new P2d(x+v.x,y+v.y);
+        return new P2d(x+v.x, y+v.y);
     }
 
     public V2d sub(P2d v){
-        return new V2d(x-v.x,y-v.y);
+        return new V2d(x-v.x, y-v.y);
     }
 
     public String toString(){
