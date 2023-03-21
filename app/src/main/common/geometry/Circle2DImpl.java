f48ca99eaa4fb877cd69a1c8468dcd827e3969ad
Fraloha <infofrancesco20@gmail.com>
2023-03-21 14:29:43 +0100

    Implemented intersect methods.

--- app/src/main/common/geometry/Circle2DImpl.java
+++ app/src/main/common/geometry/Circle2DImpl.java
@@ -1,11 +1,13 @@
 package main.common.geometry;
 
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.PathIterator;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 
 import main.common.P2d;
 
@@ -17,27 +19,40 @@ public class Circle2DImpl implements Shape {
     
     @Override
     public boolean contains(double x, double y) {
-        double xDelta = (x - this.center.x);
-        double yDelta = (y - this.center.y);
+        double xDelta = (x - this.center.getX());
+        double yDelta = (y - this.center.getY());
 
         return (xDelta*xDelta + yDelta*yDelta) <= this.radius*this.radius;
     }
-    
+
     @Override
     public boolean contains(Point2D p) {
         return this.contains(p.getX(), p.getY());   
     }
 
-    @Override
-    public boolean contains(Rectangle2D r) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    public ArrayList<Point2D> getContainedVertices(double x, double y, double w, double h) {
+
+        ArrayList<Point2D> containedVertices = new ArrayList<Point2D>(4);
+        Point2D.Double [] corners = { new Point2D.Double(x, y), new Point2D.Double(x + w, y),
+            new Point2D.Double(x, y + h), new Point2D.Double(x + w, y + h) };
+
+        for (Point2D current : corners) {
+            if (this.contains(current)) {
+                containedVertices.add(current);
+            }
+        }
+
+        return containedVertices;
     }
 
     @Override
     public boolean contains(double x, double y, double w, double h) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+        return this.getContainedVertices(x, y, w, h).size() == 4;
+    }
+
+    @Override
+    public boolean contains(Rectangle2D r) {
+        return this.contains(r.getX(), r.getY(), r.getWidth(), r.getHeight());
     }
 
     @Override
@@ -66,14 +81,12 @@ public class Circle2DImpl implements Shape {
 
     @Override
     public boolean intersects(Rectangle2D r) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'intersects'");
+        return this.intersects(r.getX(), r.getY(), r.getWidth(), r.getHeight());
     }
 
     @Override
     public boolean intersects(double x, double y, double w, double h) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'intersects'");
+        return this.getContainedVertices(x, y, w, h).size() != 0;
     }
     
 }
