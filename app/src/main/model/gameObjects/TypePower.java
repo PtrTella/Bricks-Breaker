7383b623f89007e0b59b302bc36664fc0e9476dd
Luca Bighini <bighini.luca@gmail.com>
2023-03-22 18:52:26 +0100

    Update TypePower.java

--- app/src/main/model/gameObjects/TypePower.java
+++ app/src/main/model/gameObjects/TypePower.java
@@ -1,7 +1,29 @@
 package main.model.gameObjects;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public enum TypePower {
-    NULL,
-    LONGBAR,
-    SHORTBAR
+    
+    NULL(0.0,"NULL"),
+    LONGBAR(10.0,"POSITIVE"),
+    SHORTBAR(10.0,"NEGATIVE");
+
+    public final Double timeAmount;
+    public final String type;
+
+    private TypePower(Double time, String typePower) {
+        this.timeAmount = time;
+        this.type = typePower;
+    }
+
+    public static List<TypePower> getElement(String type){
+        return Stream.of(TypePower.values()).filter(t -> t.type.equals(type)).toList();
+    }
+
+    public static void printAll() {
+        for (TypePower e: values()) {
+            System.out.println(e + "  " + e.timeAmount + " " + e.type);
+        }
+    }
 }
