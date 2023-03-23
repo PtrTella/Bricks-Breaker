dfe13ff50d852de59a84db63b8efc93ac0167f49
Fraloha <infofrancesco20@gmail.com>
2023-03-23 14:54:16 +0100

    Circle2D inherited Shape2D.

--- app/src/main/common/geometry/Circle2D.java
+++ app/src/main/common/geometry/Circle2D.java
@@ -3,19 +3,9 @@ package main.common.geometry;
 import main.common.P2d;
 import java.util.ArrayList;
 
-public interface Circle2D {
+public interface Circle2D extends Shape2D {
 
-    boolean contains(final double x, final double y);
-
-    boolean contains(final P2d p);
-
+    ArrayList<P2d> getContainedVertices(final Rectangle2D r);
+    
     ArrayList<P2d> getContainedVertices(final double x, final double y, final double w, final double h);
-
-    boolean contains(final double x, final double y, final double w, final double h);
-
-    boolean contains(final Rectangle2D r);
-
-    boolean intersects(final double x, final double y, final double w, final double h);
-
-    boolean intersects(final Rectangle2D r);
 }
