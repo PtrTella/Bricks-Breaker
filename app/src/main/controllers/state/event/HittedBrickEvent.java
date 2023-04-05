cb9aaaab538af1a4143466011da797cf46b77c6e
Fraloha <infofrancesco20@gmail.com>
2023-04-05 10:50:59 +0200

    Created HittedBrickEvent.java

---
+++ app/src/main/controllers/state/event/HittedBrickEvent.java
@@ -0,0 +1,23 @@
+package main.controllers.state.event;
+
+import main.model.GameState;
+import main.model.gameObjects.Brick;
+
+public class HittedBrickEvent implements WorldEvent {
+
+    private Brick hittedBrick;
+
+    public HittedBrickEvent(Brick hitted) {
+        this.hittedBrick = hitted;
+    }
+
+    @Override
+    public void process(GameState currentGame) {
+        this.hittedBrick.decLife();
+
+        if (this.hittedBrick.getLife() <= 0) {
+            currentGame.getWorld().getBricks().remove(this.hittedBrick);
+        }
+    }
+    
+}
