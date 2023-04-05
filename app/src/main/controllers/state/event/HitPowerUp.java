3fc0f99a6b8a4c6f551a1347f92a43908aefd305
Fraloha <infofrancesco20@gmail.com>
2023-04-05 10:50:43 +0200

    Creted HitPowerUp event.

---
+++ app/src/main/controllers/state/event/HitPowerUp.java
@@ -0,0 +1,23 @@
+package main.controllers.state.event;
+
+import main.model.GameState;
+import main.model.gameObjects.PowerUp;
+
+public class HitPowerUp implements WorldEvent{
+
+    private PowerUp picked;
+
+    public HitPowerUp(final PowerUp p) {
+        this.picked = p;
+    }
+
+    public PowerUp getPickedPowerUp() {
+        return this.picked;
+    }
+
+    @Override
+    public void process(final GameState currentGame) {
+        //TODO: applyPowerUp() method in GameState(?)
+    }
+    
+}
