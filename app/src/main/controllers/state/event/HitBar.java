63b2e7b581227a483273461b902ac37f69f040ff
Luca Bighini <bighini.luca@gmail.com>
2023-04-06 17:12:14 +0200

    Create HitBar.java

---
+++ app/src/main/controllers/state/event/HitBar.java
@@ -0,0 +1,13 @@
+package main.controllers.state.event;
+
+import main.model.GameState;
+
+public class HitBar implements HitObjects {
+
+    @Override
+    public void process(GameState currentGame) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'process'");
+    }
+    
+}
