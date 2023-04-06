b21ba26080b2799ce821af9fd22dfb3ebb6c4d4d
Luca Bighini <bighini.luca@gmail.com>
2023-04-06 19:04:58 +0200

    TODO in HitBar

--- app/src/main/controllers/state/event/HitBar.java
+++ app/src/main/controllers/state/event/HitBar.java
@@ -1,13 +1,19 @@
 package main.controllers.state.event;
 
 import main.model.GameState;
+import main.model.gameObjects.Ball;
 
 public class HitBar implements HitObjects {
 
+    private Ball ball;
+
+    public HitBar(Ball ball){
+        this.ball = ball;
+    }
+
     @Override
     public void process(GameState currentGame) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'process'");
+        // TODO invert vel of ball with corner radius
     }
     
 }
