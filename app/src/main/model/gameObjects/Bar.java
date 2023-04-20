8c9a63a2cefd451693baed73eb9f1112f7b53ab9
Fraloha <infofrancesco20@gmail.com>
2023-04-20 16:42:12 +0200

    Added width as an attribute.

--- app/src/main/model/gameObjects/Bar.java
+++ app/src/main/model/gameObjects/Bar.java
@@ -9,8 +9,18 @@ public class Bar extends GameObjectImpl<RectBoundingBox>{
     private static final Double BAR_WIDTH = 5.0;
     private static final Double BAR_HEIGHT = 1.0;
 
+    private Double width;
+
     public Bar(P2d pos, V2d vel, final Integer lifeToset) {
         super(lifeToset, vel, WorldImpl.TypeObj.BAR, new RectBoundingBox(pos, BAR_WIDTH, BAR_HEIGHT));
+        this.width = BAR_WIDTH;
     }
-    
+
+    public Double getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(final Double widthToSet) {
+        this.width = widthToSet;
+    }
 }
