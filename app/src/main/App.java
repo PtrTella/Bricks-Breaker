cbd71315e698dc2c620ff6073fd4b390cd89fc3f
Luca Bighini <bighini.luca@gmail.com>
2023-04-24 16:34:37 +0200

    Update App.java

--- app/src/main/App.java
+++ app/src/main/App.java
@@ -1,11 +1,7 @@
 package main;
 
-import main.core.GameEngine;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        GameEngine engine = new GameEngine();
-	    engine.initGame();
-	    //engine.mainLoop();
+        
     }
 }
